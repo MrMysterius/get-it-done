bddf7cdfa2881ae0d6dd2f7514136ff01b7ce27d
MrMysterius <official.mrmysterius@gmail.com>
2024-01-10 16:29:59 +0100

    feature: adding keys to the gid_info table during database creation

--- src/functions/createDatabase.ts
+++ src/functions/createDatabase.ts
@@ -14,6 +14,14 @@ export function createDatabase() {
     ? null
     : process.exit(100);
 
+  console.log("+ Inserting Keys into gid_info");
+  {
+    const info_insert = `INSERT INTO gid_info (key, value) VALUES (?, ?)`;
+    executeRawStatement(info_insert, "version", "0.1.0");
+    executeRawStatement(info_insert, "installation_timestamp", Date.now().toString());
+    executeRawStatement(info_insert, "last_startup_timestamp", Date.now().toString());
+  }
+
   console.log("# Finished Creating Database");
   console.log("----------------------------");
   return true;
