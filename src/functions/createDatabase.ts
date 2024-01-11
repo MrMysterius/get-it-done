efec013e6a59e8b4a0c5649e5a47f2aa4a5e6523
MrMysterius <official.mrmysterius@gmail.com>
2024-01-11 07:49:56 +0100

    fix: database creation updated to create tables - user, groups, group_members, filters, tags, states, tasks, task_tags, task_state

--- src/functions/createDatabase.ts
+++ src/functions/createDatabase.ts
@@ -22,6 +22,120 @@ export function createDatabase() {
     statement.run("last_startup_timestamp", Date.now().toString());
   }
 
+  console.log("+ Table users");
+  executeRawStatement(
+    `CREATE TABLE "users" (
+      "user_id"	INTEGER,
+      "user_name"	TEXT NOT NULL UNIQUE,
+      "user_password_hash" TEXT NOT NULL,
+      "user_displayname"	TEXT NOT NULL,
+      "user_last_action_timestamp"	TEXT,
+      "user_active"	INTEGER NOT NULL,
+      PRIMARY KEY("user_id" AUTOINCREMENT)
+    );`
+  );
+
+  console.log("+ Table groups");
+  executeRawStatement(
+    `CREATE TABLE "groups" (
+      "group_id"	INTEGER,
+      "group_name"	TEXT NOT NULL,
+      "group_owner"	INTEGER,
+      FOREIGN KEY("group_owner") REFERENCES "users"("user_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      PRIMARY KEY("group_id" AUTOINCREMENT)
+    );`
+  );
+
+  console.log("+ Table group_members");
+  executeRawStatement(
+    `CREATE TABLE "group_members" (
+      "group_id"	INTEGER,
+      "user_id"	INTEGER,
+      FOREIGN KEY("user_id") REFERENCES "users"("user_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      FOREIGN KEY("group_id") REFERENCES "groups"("group_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      PRIMARY KEY("group_id","user_id")
+    );`
+  );
+
+  console.log("+ Table filters");
+  executeRawStatement(
+    `CREATE TABLE "filters" (
+      "filter_id"	INTEGER,
+      "user_id"	INTEGER,
+      "filter_data"	TEXT NOT NULL,
+      FOREIGN KEY("user_id") REFERENCES "users"("user_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      PRIMARY KEY("filter_id" AUTOINCREMENT)
+    );`
+  );
+
+  console.log("+ Table states");
+  executeRawStatement(
+    `CREATE TABLE "states" (
+      "state_id"	INTEGER,
+      "group_id"	INTEGER,
+      "state_name"	TEXT NOT NULL,
+      "state_description"	TEXT DEFAULT '',
+      "state_colour_text"	TEXT NOT NULL DEFAULT '#000000',
+      "state_colour_background"	TEXT NOT NULL DEFAULT '#262626',
+      FOREIGN KEY("group_id") REFERENCES "groups"("group_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      PRIMARY KEY("state_id" AUTOINCREMENT)
+    );`
+  );
+
+  console.log("+ Table tags");
+  executeRawStatement(
+    `CREATE TABLE "tags" (
+      "tag_id"	INTEGER,
+      "group_id"	INTEGER,
+      "tag_name"	TEXT NOT NULL,
+      "tag_description"	TEXT DEFAULT '',
+      "tag_type"	TEXT NOT NULL,
+      "tag_colour_text"	TEXT NOT NULL DEFAULT '#000000',
+      "tag_colour_background"	TEXT NOT NULL DEFAULT '#66a3ff',
+      FOREIGN KEY("group_id") REFERENCES "groups"("group_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      PRIMARY KEY("tag_id" AUTOINCREMENT)
+    );`
+  );
+
+  console.log("+ Table tasks");
+  executeRawStatement(
+    `CREATE TABLE "tasks" (
+      "task_id"	INTEGER,
+      "group_id"	INTEGER,
+      "task_title"	TEXT,
+      "task_description"	TEXT,
+      "task_date_start"	TEXT,
+      "task_date_due"	TEXT,
+      "task_time_estimate"	INTEGER,
+      "task_time_needed"	INTEGER,
+      "task_archived"	INTEGER NOT NULL DEFAULT 0,
+      FOREIGN KEY("group_id") REFERENCES "groups"("group_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      PRIMARY KEY("task_id" AUTOINCREMENT)
+    );`
+  );
+
+  console.log("+ Table task_tags");
+  executeRawStatement(
+    `CREATE TABLE "task_tags" (
+      "task_id"	INTEGER,
+      "tag_id"	INTEGER,
+      FOREIGN KEY("tag_id") REFERENCES "tags"("tag_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      FOREIGN KEY("task_id") REFERENCES "tasks"("task_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      PRIMARY KEY("task_id","tag_id")
+    );`
+  );
+
+  console.log("+ Table task_state");
+  executeRawStatement(
+    `CREATE TABLE "task_state" (
+      "task_id"	INTEGER UNIQUE,
+      "state_id"	INTEGER,
+      FOREIGN KEY("state_id") REFERENCES "states"("state_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      FOREIGN KEY("task_id") REFERENCES "tasks"("task_id") ON UPDATE CASCADE ON DELETE CASCADE,
+      PRIMARY KEY("task_id","state_id")
+    );`
+  );
+
   console.log("# Finished Creating Database");
   console.log("----------------------------");
   return true;
