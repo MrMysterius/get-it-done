8a68ca7dbc54ecf48d25569c848e6c2c3a331ee7
MrMysterius <official.mrmysterius@gmail.com>
2024-01-15 07:58:04 +0100

    feature: added validateData middleware to validate if the request got valid data

---
+++ src/middlewares/validateData.ts
@@ -0,0 +1,13 @@
+import Express from "express";
+import { generateErrorWithStatus } from "../functions/generateErrorWithStatus";
+import { validationResult } from "express-validator";
+
+export function validateData(req: Express.Request, res: Express.Response, next: Express.NextFunction) {
+  const result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    throw generateErrorWithStatus("Bad Request", 401, { errors: result.array() });
+  }
+
+  next();
+}
