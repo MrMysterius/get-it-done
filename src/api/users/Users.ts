6fbcecb7540c1624284395bc2fb68ff1c3faca2a
MrMysterius <official.mrmysterius@gmail.com>
2024-01-15 21:03:29 +0100

    feature: added POST /users route for adding users as an admin

--- src/api/users/Users.ts
+++ src/api/users/Users.ts
@@ -1,6 +1,8 @@
 import Express from "express";
 import { UsersGetRouter } from "./UsersGet";
+import { UsersPostRouter } from "./UsersPost";
 
 export const UsersRouter = Express.Router();
 
 UsersRouter.use("/", UsersGetRouter);
+UsersRouter.use("/", UsersPostRouter);
