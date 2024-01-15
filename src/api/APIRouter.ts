488dc7500ce9eb65b242ca4054df515c35804e97
MrMysterius <official.mrmysterius@gmail.com>
2024-01-15 06:18:37 +0100

    feature: added check for needed authentication for all api endpoints in general

--- src/api/APIRouter.ts
+++ src/api/APIRouter.ts
@@ -1,6 +1,8 @@
 import Express from "express";
+import { checkAuthStatus } from "../functions/checkAuthstatus";
 import { validateAuth } from "../functions/validateAuth";
 
 export const APIRouter = Express.Router();
 
 APIRouter.use(validateAuth);
+APIRouter.use(checkAuthStatus());
