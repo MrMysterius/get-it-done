dc50b8e3f5472e0ee6546def8295e6e737a9ecb6
MrMysterius <official.mrmysterius@gmail.com>
2024-01-17 10:30:46 +0100

    feature: added route PUT /users/:user_id for updating a user as admin or the user itself

--- src/api/users/Users.ts
+++ src/api/users/Users.ts
@@ -1,8 +1,10 @@
 import Express from "express";
 import { UsersGetRouter } from "./UsersGet";
 import { UsersPostRouter } from "./UsersPost";
+import { UsersPutRouter } from "./UsersPut";
 
 export const UsersRouter = Express.Router();
 
 UsersRouter.use("/", UsersGetRouter);
 UsersRouter.use("/", UsersPostRouter);
+UsersRouter.use("/", UsersPutRouter);
