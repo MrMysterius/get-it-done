c049dbd3d0a40b7942803ca5938c8546460a0118
MrMysterius <official.mrmysterius@gmail.com>
2024-01-18 18:05:20 +0100

    feature: added route GET /groups and GET /groups/:group_id for getting a list of groups from a user/all users

--- src/api/groups/Groups.ts
+++ src/api/groups/Groups.ts
@@ -1,8 +1,10 @@
 import Express from "express";
+import { GroupsGetRouter } from "./GroupsGet";
 import { GroupsPostRouter } from "./GroupsPost";
 import { GroupsPutRouter } from "./GroupsPut";
 
 export const GroupsRouter = Express.Router();
 
+GroupsRouter.use("/", GroupsGetRouter);
 GroupsRouter.use("/", GroupsPostRouter);
 GroupsRouter.use("/", GroupsPutRouter);
