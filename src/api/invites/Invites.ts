e92f17eb75c1c0174c7121ecdb34e621fbcbc097
MrMysterius <official.mrmysterius@gmail.com>
2024-01-18 11:40:41 +0100

    feature: added route GET /invites and GET /invites/:invite_id for getting all invites a user / all users have and a specific one

--- src/api/invites/Invites.ts
+++ src/api/invites/Invites.ts
@@ -1,6 +1,8 @@
 import Express from "express";
+import { InvitesGetRouter } from "./InvitesGet";
 import { InvitesPostRouter } from "./InvitesPost";
 
 export const InvitesRouter = Express.Router();
 
+InvitesRouter.use("/", InvitesGetRouter);
 InvitesRouter.use("/", InvitesPostRouter);
