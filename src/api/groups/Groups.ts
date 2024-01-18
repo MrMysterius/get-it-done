36c7600a1100f5e33c45374ace6b33b176c9f804
MrMysterius <official.mrmysterius@gmail.com>
2024-01-18 17:31:05 +0100

    feature: added route PUT /groups/:group_id for updating a group

--- src/api/groups/Groups.ts
+++ src/api/groups/Groups.ts
@@ -1,6 +1,8 @@
 import Express from "express";
 import { GroupsPostRouter } from "./GroupsPost";
+import { GroupsPutRouter } from "./GroupsPut";
 
 export const GroupsRouter = Express.Router();
 
 GroupsRouter.use("/", GroupsPostRouter);
+GroupsRouter.use("/", GroupsPutRouter);
