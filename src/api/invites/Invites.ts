15b4548c2358dd25fc4a8a3c205d23fa07437461
MrMysterius <official.mrmysterius@gmail.com>
2024-01-18 13:29:21 +0100

    feature: added route PUT /invites/:invite_id for updating a invite

--- src/api/invites/Invites.ts
+++ src/api/invites/Invites.ts
@@ -2,9 +2,11 @@ import Express from "express";
 import { InvitesDeleteRouter } from "./InvitesDelete";
 import { InvitesGetRouter } from "./InvitesGet";
 import { InvitesPostRouter } from "./InvitesPost";
+import { InvitesPutRouter } from "./InvitesPut";
 
 export const InvitesRouter = Express.Router();
 
 InvitesRouter.use("/", InvitesGetRouter);
 InvitesRouter.use("/", InvitesPostRouter);
+InvitesRouter.use("/", InvitesPutRouter);
 InvitesRouter.use("/", InvitesDeleteRouter);
