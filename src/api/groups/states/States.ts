a1f4d48ffee270d9248328886aae9cfcc03ce126
MrMysterius <official.mrmysterius@gmail.com>
2024-01-19 12:17:43 +0100

    feature: added route PUT /groups/:group_id/states/:state_id for updating a state

--- src/api/groups/states/States.ts
+++ src/api/groups/states/States.ts
@@ -1,6 +1,8 @@
 import Express from "express";
 import { StatesPostRouter } from "./StatesPost";
+import { StatesPutRouter } from "./StatesPut";
 
 export const StatesRouter = Express.Router();
 
 StatesRouter.use("/", StatesPostRouter);
+StatesRouter.use("/", StatesPutRouter);
