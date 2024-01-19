a7ddc5bced3564e0131ce26cd29cb032d1727d8a
MrMysterius <official.mrmysterius@gmail.com>
2024-01-19 10:26:46 +0100

    feature: added route GET /groups/:group_id/members for getting a list of group members

--- src/api/groups/members/Members.ts
+++ src/api/groups/members/Members.ts
@@ -1,6 +1,8 @@
 import Express from "express";
+import { MembersGetRouter } from "./MembersGet";
 import { MembersPostRouter } from "./MembersPost";
 
 export const MembersRouter = Express.Router();
 
+MembersRouter.use("/", MembersGetRouter);
 MembersRouter.use("/", MembersPostRouter);
