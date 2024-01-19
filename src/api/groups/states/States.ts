b046c21008ccb8cf14987d6a8cf5d49af87d5a1b
MrMysterius <official.mrmysterius@gmail.com>
2024-01-19 12:28:02 +0100

    feature: added routes GET /groups/:group_id/states and GET /groups/:group_id/states/:state_id for listing states of a group

--- src/api/groups/states/States.ts
+++ src/api/groups/states/States.ts
@@ -1,8 +1,10 @@
 import Express from "express";
+import { StatesGetRouter } from "./StatesGet";
 import { StatesPostRouter } from "./StatesPost";
 import { StatesPutRouter } from "./StatesPut";
 
 export const StatesRouter = Express.Router();
 
+StatesRouter.use("/", StatesGetRouter);
 StatesRouter.use("/", StatesPostRouter);
 StatesRouter.use("/", StatesPutRouter);
