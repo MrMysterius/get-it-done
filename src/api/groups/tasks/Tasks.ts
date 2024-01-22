3b86b6e1ba4d05cb7decf44f195324deaf817af0
MrMysterius <official.mrmysterius@gmail.com>
2024-01-22 12:59:20 +0100

    feature: added route PUT /groups/:group_id/tasks/:task_id for updating a task

--- src/api/groups/tasks/Tasks.ts
+++ src/api/groups/tasks/Tasks.ts
@@ -1,6 +1,8 @@
 import Express from "express";
 import { TasksPostRouter } from "./TasksPost";
+import { TasksPutRouter } from "./TasksPut";
 
 export const TasksRouter = Express.Router();
 
 TasksRouter.use("/", TasksPostRouter);
+TasksRouter.use("/", TasksPutRouter);
