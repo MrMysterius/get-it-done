f4c3b0fbc89423d29ab4aba00890fe8aafcedb4c
MrMysterius <official.mrmysterius@gmail.com>
2024-01-24 15:40:28 +0100

    feature: added TasksSubRouter which adds also task data into the request for further processing

--- src/api/groups/tasks/Tasks.ts
+++ src/api/groups/tasks/Tasks.ts
@@ -3,6 +3,8 @@ import { TasksDeleteRouter } from "./TasksDelete";
 import { TasksGetRouter } from "./TasksGet";
 import { TasksPostRouter } from "./TasksPost";
 import { TasksPutRouter } from "./TasksPut";
+import { getData } from "@/functions/databaseFunctions";
+import { param } from "express-validator";
 
 export const TasksRouter = Express.Router();
 
@@ -10,3 +12,25 @@ TasksRouter.use("/", TasksGetRouter);
 TasksRouter.use("/", TasksPostRouter);
 TasksRouter.use("/", TasksPutRouter);
 TasksRouter.use("/", TasksDeleteRouter);
+
+export const TasksSubRouter = Express.Router();
+
+TasksRouter.use(
+  "/:task_id",
+  param("task_id")
+    .trim()
+    .notEmpty()
+    .isNumeric()
+    .toInt()
+    .custom((id: string, meta) => {
+      const req = meta.req as Express.Request;
+      const task = getData<GIDData.task>(`SELECT * FROM tasks WHERE task_id = ?`, id);
+      if (!task.data || task.data.task_creator != req.extra.params.group_id) throw new Error("Tag with that id doesn't exist");
+
+      req.extra.params.task_id = task.data.task_id;
+      req.extra.task = task.data;
+
+      return true;
+    }),
+  TasksSubRouter
+);
