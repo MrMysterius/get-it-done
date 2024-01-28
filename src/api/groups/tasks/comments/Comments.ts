5c573b5538237d11f8bf56544f8fb2599c89cf87
MrMysterius <official.mrmysterius@gmail.com>
2024-01-28 12:47:08 +0100

    feature: added route for updating a task comment

--- src/api/groups/tasks/comments/Comments.ts
+++ src/api/groups/tasks/comments/Comments.ts
@@ -1,8 +1,10 @@
 import { CommentsGetRouter } from "./CommentsGet";
 import { CommentsPostRouter } from "./CommentsPost";
+import { CommentsPutRouter } from "./CommentsPut";
 import Express from "express";
 
 export const CommentsRouter = Express.Router();
 
 CommentsRouter.use("/", CommentsGetRouter);
 CommentsRouter.use("/", CommentsPostRouter);
+CommentsRouter.use("/", CommentsPutRouter);
