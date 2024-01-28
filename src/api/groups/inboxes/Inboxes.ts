0ec3d497bb1aaea857f2cb61df99701712f1dc81
MrMysterius <official.mrmysterius@gmail.com>
2024-01-28 15:31:02 +0100

    feature: added route for getting inbox codes from a group

--- src/api/groups/inboxes/Inboxes.ts
+++ src/api/groups/inboxes/Inboxes.ts
@@ -1,6 +1,8 @@
 import Express from "express";
+import { InboxesGetRouter } from "./InboxesGet";
 import { InboxesPostRouter } from "./InboxesPost";
 
 export const InboxesRouter = Express.Router();
 
+InboxesRouter.use("/", InboxesGetRouter);
 InboxesRouter.use("/", InboxesPostRouter);
