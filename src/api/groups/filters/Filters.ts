4c0c4727d4e0e06d21ea43b5a85b6143748c892d
MrMysterius <official.mrmysterius@gmail.com>
2024-01-28 16:29:48 +0100

    feature: added route for deleting filter from a group

--- src/api/groups/filters/Filters.ts
+++ src/api/groups/filters/Filters.ts
@@ -1,4 +1,5 @@
 import Express from "express";
+import { FiltersDeleteRouter } from "./FiltersDelete";
 import { FiltersGetRouter } from "./FiltersGet";
 import { FiltersPostRouter } from "./FiltersPost";
 import { FiltersPutRouter } from "./FiltersPut";
@@ -8,3 +9,4 @@ export const FiltersRouter = Express.Router();
 FiltersRouter.use("/", FiltersGetRouter);
 FiltersRouter.use("/", FiltersPostRouter);
 FiltersRouter.use("/", FiltersPutRouter);
+FiltersRouter.use("/", FiltersDeleteRouter);
