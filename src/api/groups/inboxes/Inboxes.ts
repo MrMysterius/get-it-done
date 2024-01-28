475d22465e4580082c370f47e5a42181d5b9edfb
MrMysterius <official.mrmysterius@gmail.com>
2024-01-28 15:48:11 +0100

    feature: added route for updating inbox code on a group

--- src/api/groups/inboxes/Inboxes.ts
+++ src/api/groups/inboxes/Inboxes.ts
@@ -1,8 +1,10 @@
 import Express from "express";
 import { InboxesGetRouter } from "./InboxesGet";
 import { InboxesPostRouter } from "./InboxesPost";
+import { InboxesPutRouter } from "./InboxesPut";
 
 export const InboxesRouter = Express.Router();
 
 InboxesRouter.use("/", InboxesGetRouter);
 InboxesRouter.use("/", InboxesPostRouter);
+InboxesRouter.use("/", InboxesPutRouter);
