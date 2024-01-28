f453f5d58f64dd2c4d9117f4d6a88279a4b5ade4
MrMysterius <official.mrmysterius@gmail.com>
2024-01-28 12:20:51 +0100

    feature: added route for getting comments on a task

--- src/api/groups/tasks/comments/Comments.ts
+++ src/api/groups/tasks/comments/Comments.ts
@@ -1,6 +1,8 @@
+import { CommentsGetRouter } from "./CommentsGet";
 import { CommentsPostRouter } from "./CommentsPost";
 import Express from "express";
 
 export const CommentsRouter = Express.Router();
 
+CommentsRouter.use("/", CommentsGetRouter);
 CommentsRouter.use("/", CommentsPostRouter);
---
+++ src/api/groups/tasks/comments/CommentsGet.ts
@@ -0,0 +1,45 @@
+import Express from "express";
+import { getAllData } from "@/functions/databaseFunctions";
+import { validateData } from "@/middlewares/validateData";
+
+export const CommentsGetRouter = Express.Router();
+
+CommentsGetRouter.get(
+  "/",
+
+  // REQUEST DATA REQUIREMENTS
+
+  // DATA CHECK
+  validateData,
+
+  // ACTUAL REQUEST HANDLE
+  (req, res) => {
+    const comments = getAllData<GIDData.comment & Pick<GIDData.user, "user_name">>(
+      `SELECT
+        comments.*,
+        users.user_name
+      FROM comments
+      LEFT JOIN users
+      ON comments.user_id = users.user_id
+      WHERE task_id = ?`,
+      req.extra.params.task_id
+    );
+
+    if (!comments.data) throw new Error(`Couldn't get comments`);
+
+    res.status(200);
+    res.send(
+      comments.data.map((comment) => {
+        return {
+          comment_id: comment.comment_id,
+          comment: comment.comment,
+          last_changed: comment.comment_last_changed,
+          user: {
+            id: comment.user_id,
+            name: comment.user_name,
+          },
+        };
+      })
+    );
+  }
+);
