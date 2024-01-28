3c2431e6d2e4165e36018d3948944a420dd8e8c6
MrMysterius <official.mrmysterius@gmail.com>
2024-01-28 16:25:20 +0100

    feature: added route for getting filters from a group

--- src/api/groups/filters/Filters.ts
+++ src/api/groups/filters/Filters.ts
@@ -1,8 +1,10 @@
 import Express from "express";
+import { FiltersGetRouter } from "./FiltersGet";
 import { FiltersPostRouter } from "./FiltersPost";
 import { FiltersPutRouter } from "./FiltersPut";
 
 export const FiltersRouter = Express.Router();
 
+FiltersRouter.use("/", FiltersGetRouter);
 FiltersRouter.use("/", FiltersPostRouter);
 FiltersRouter.use("/", FiltersPutRouter);
