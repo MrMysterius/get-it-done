82ac1c372d1892be446e8ae01c1fa3bc7ba40a31
MrMysterius <official.mrmysterius@gmail.com>
2024-01-29 11:33:38 +0100

    fix: servable file finder not having a ressource and completly breaking

--- src/functions/findServableFile.ts
+++ src/functions/findServableFile.ts
@@ -9,19 +9,19 @@ export function findServableFile(start_path: string, req: Express.Request) {
   const resource = req.baseUrl.match(/(?<parent>^.*\/)(?<resource>[^\/]*$)/);
   const potentialFilePaths: Array<string> = [];
 
-  if (!resource) throw generateErrorWithStatus("Couldn't filter out ressource", 500);
+  // if (!resource) throw generateErrorWithStatus("Couldn't filter out ressource", 500);
 
   logger.debug(resource);
 
   if (req.isAuthed) {
-    const basicAuthedPath = path.join(start_path, `${resource.groups?.parent}`, `authed.${resource.groups?.resource}`);
-    if (resource.groups?.resource != "") {
+    if (resource && resource.groups?.resource != "") {
+      const basicAuthedPath = path.join(start_path, `${resource.groups?.parent}`, `authed.${resource.groups?.resource}`);
       potentialFilePaths.push(basicAuthedPath, `${basicAuthedPath}.html`, `${basicAuthedPath}.md`);
     }
     potentialFilePaths.push(path.join(literalPath, `authed.index.html`));
   }
 
-  if (resource.groups?.resource != "") {
+  if (resource && resource.groups?.resource != "") {
     potentialFilePaths.push(literalPath, `${literalPath}.html`, `${literalPath}.md`);
   }
   potentialFilePaths.push(path.join(literalPath, `index.html`));
