e4409627e0216fae96e4ffd54224281809d1f576
MrMysterius <official.mrmysterius@gmail.com>
2024-01-31 12:39:10 +0100

    feature front: added function to check authed users groups (and select the first)

---
+++ src/serve/scripts/functions/authed.checkGroup.js
@@ -0,0 +1,22 @@
+import { getUrlParam, setUrlParam } from "./authed.urlData.js";
+
+import { createNotice } from "./authed.createNotice.js";
+import { request } from "./authed.request.js";
+
+export async function checkGroup(selectFirst = true) {
+  const group_id = getUrlParam("g");
+  const groups = await request("GET", "/api/groups");
+  if (!groups || groups.status != 200) {
+    createNotice("Couldn't get groups");
+    return false;
+  }
+
+  const existing_group = groups.data.sort((a, b) => a.id - b.id).find((group) => group.group_id == group_id);
+
+  if (selectFirst && !existing_group) {
+    setUrlParam("g", groups.data[0]?.group_id || null);
+    return true;
+  }
+
+  return true;
+}
