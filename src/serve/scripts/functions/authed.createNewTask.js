6cea88ba792a5a84f623127bb42f8b8d5dbdfd14
MrMysterius <official.mrmysterius@gmail.com>
2024-01-31 12:42:49 +0100

    feature front: added function to create new tasks

---
+++ src/serve/scripts/functions/authed.createNewTask.js
@@ -0,0 +1,23 @@
+import { createNotice } from "./authed.createNotice.js";
+import { getUrlParam } from "./authed.urlData.js";
+import { request } from "./authed.request.js";
+
+export async function createNewTask() {
+  const titleEl = document.querySelector("#task-new-title");
+  const title = titleEl?.value || null;
+  if (!title) return false;
+
+  const res = await request("POST", `/api/groups/${getUrlParam("g")}/tasks`, {
+    title,
+  });
+
+  if (!res || res.status != 200) {
+    createNotice("Couldn't create new Task", "error", 30000);
+    return false;
+  }
+
+  titleEl.value = "";
+
+  createNotice("Task Created", "success", 5000);
+  return true;
+}
