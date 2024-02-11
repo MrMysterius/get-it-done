81027d878a414b89851e5ca954b0dd3f376df723
MrMysterius <official.mrmysterius@gmail.com>
2024-02-11 21:39:01 +0100

    feature frontend: auto apply filter tags to a new task

--- src/serve/scripts/functions/authed.createNewTask.js
+++ src/serve/scripts/functions/authed.createNewTask.js
@@ -3,11 +3,18 @@ import { getUrlParam } from "./authed.urlData.js";
 import { request } from "./request.js";
 
 export async function createNewTask() {
+  const group_id = getUrlParam("g");
   const titleEl = document.querySelector("#task-new-title");
   const title = titleEl?.value || null;
   if (!title) return false;
 
-  const res = await request("POST", `/api/groups/${getUrlParam("g")}/tasks`, {
+  let filters = await request("GET", `/api/groups/${group_id}/filters`);
+  if (!filters || filters.status != 200) {
+    filters = { data: [] };
+  }
+  const filter = filters.data.find((f) => f.id == getUrlParam("f"));
+
+  const res = await request("POST", `/api/groups/${group_id}/tasks`, {
     title,
   });
 
@@ -15,6 +22,11 @@ export async function createNewTask() {
     createNotice("Couldn't create new Task", "error", 30000);
     return false;
   }
+
+  if (filter)
+    await request("POST", `/api/groups/${group_id}/tasks/${res.data.task_id}/tags`, {
+      tags: filter.filter_data.tags,
+    });
 
   titleEl.value = "";
 
