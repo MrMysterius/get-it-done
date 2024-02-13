433bd7cbb50de9115313c1d78705456e53a16043
MrMysterius <official.mrmysterius@gmail.com>
2024-02-13 17:27:01 +0100

    chore: updated changelog route

--- src/api/ChangelogGet.ts
+++ src/api/ChangelogGet.ts
@@ -8,8 +8,23 @@ ChangelogGetRouter.get("/", (req, res) => {
     changelog: [
       {
         version: "0.1.1",
+        title: "",
+        description: "",
         changes: {
           new_features: ["Added Changelog Endpoint /api/changelog"],
+          improvements: [],
+          fixes: [],
+          refactors: [],
+          deprecations: [],
+          removed: [],
+        },
+      },
+      {
+        version: "0.1.0",
+        title: "ALPHA Release",
+        description: "A usable Version of the Project. That can be tested from testers.",
+        changes: {
+          new_features: ["Added the Backend for interacting with stored data anywhere.", "Added Frontend for Interacting with the Backend as a Human."],
           improvements: [],
           fixes: [],
           refactors: [],
