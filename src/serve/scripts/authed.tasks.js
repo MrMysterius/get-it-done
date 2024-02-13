0ddbb462b43f5f8307a228fffcba712906603b5e
MrMysterius <official.mrmysterius@gmail.com>
2024-02-13 17:44:01 +0100

    feature frontend: changelog check and popup display of last changelog

--- src/serve/scripts/authed.tasks.js
+++ src/serve/scripts/authed.tasks.js
@@ -1,5 +1,6 @@
 import { getUrlParam, setUrlParam } from "./functions/authed.urlData.js";
 
+import { checkChangelog } from "./functions/authed.checkChangelog.js";
 import { checkGroup } from "./functions/authed.checkGroup.js";
 import { createFilterPopup } from "./functions/authed.createFilterPopup.js";
 import { createNewTask } from "./functions/authed.createNewTask.js";
@@ -17,6 +18,7 @@ import { switchGroup } from "./functions/authed.switchGroup.js";
 export let loop_interval_id;
 
 window.addEventListener("DOMContentLoaded", async (ev) => {
+  checkChangelog();
   await checkGroup();
   await populateGroups();
   switchGroup();
