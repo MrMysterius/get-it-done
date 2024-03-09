373e8c8c4098e392ee3486eaf3d29f67c51a1d89
Mr Mysterius <official.mrmysterius@gmail.com>
2024-03-09 16:33:57 +0100

    refactor backend: function for updating a users last action timestamp
    the function know uses types from the types declaration and and has the better statement creation

--- src/functions/updateLastActionTimestamp.ts
+++ src/functions/updateLastActionTimestamp.ts
@@ -1,10 +1,16 @@
-import { createTransactionStatement } from "./databaseFunctions";
+import { createTransactionStatementTyped } from "./databaseFunctions";
 
-export function updateLastActionTimestamp(user_id: number) {
-  const statement = createTransactionStatement(`UPDATE users SET user_last_action_timestamp = ? WHERE user_id = ?`);
-  const res = statement.run(Date.now().toString(), user_id);
-  if (!res.isSuccessful) {
-    throw new Error(`Couldn't update user ${user_id}'s last action timestamp`);
+export function updateLastActionTimestamp(user_id: GIDData.user["user_id"]) {
+  const updateTimestamp = createTransactionStatementTyped<Pick<GIDData.user, "user_id" | "user_last_action_timestamp">>(
+    `UPDATE users
+    SET user_last_action_timestamp = @user_last_action_timestamp
+    WHERE user_id = @user_id`
+  );
+
+  const updateTimestampResponse = updateTimestamp.run({ user_id: user_id, user_last_action_timestamp: Date.now().toString() });
+
+  if (!updateTimestampResponse.isSuccessful) {
+    throw new Error(`Couldn't update the timestamp of "${user_id}"'s last action.`);
   }
-  return res;
+  return updateTimestampResponse;
 }
