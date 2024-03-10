43edce659dbc618df57c88f8b2872d1257c33916
MrMysterius <official.mrmysterius@gmail.com>
2024-03-10 17:21:41 +0100

    feature: added reusable queries for users with invitee

---
+++ src/database-queries/userQueries.ts
@@ -0,0 +1,22 @@
+import { createDataQuery } from "@/functions/databaseFunctions";
+
+export const getAllUsersWithInvitee = createDataQuery<GIDData.user & { invitee_user_name: string; invitee_user_displayname: string }>()(
+  `SELECT
+    users.*,
+    invite.user_name as invitee_user_name,
+    invite.user_displayname as invitee_user_displayname
+  FROM users
+  LEFT JOIN users as invite
+  ON invite.user_id = users.user_invited_from`
+);
+
+export const getUserWithInvitee = createDataQuery<GIDData.user & { invitee_user_name: string; invitee_user_displayname: string }>()(
+  `SELECT
+    users.*,
+    invite.user_name as invitee_user_name,
+    invite.user_displayname as invitee_user_displayname
+  FROM users
+  LEFT JOIN users as invite
+  ON invite.user_id = users.user_invited_from
+  WHERE users.user_id = @user_id`
+);
