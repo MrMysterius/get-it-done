a23f65cfb156b84780ba424c7d43bc399a267384
MrMysterius <official.mrmysterius@gmail.com>
2024-03-14 17:42:58 +0100

    refactor: users get endpoint responses

--- src/functions/database/queries/users.ts
+++ src/functions/database/queries/users.ts
@@ -3,3 +3,30 @@ import { DBQuery } from "../Query";
 export const getUserByID = new DBQuery<Pick<GIDData.user, "user_id">, GIDData.user>(`SELECT * FROM "users" WHERE "user_id" = @user_id`);
 
 export const getUserByName = new DBQuery<Pick<GIDData.user, "user_name">, GIDData.user>(`SELECT * FROM "users" WHERE "user_name" = @user_name`);
+
+export const getUsersWithInvitee = new DBQuery<
+  {},
+  GIDData.user & { invitee_user_name: GIDData.user["user_name"]; invitee_user_displayname: GIDData.user["user_displayname"] }
+>(
+  `SELECT
+    users.*,
+    invite.user_name as invitee_user_name,
+    invite.user_displayname as invitee_user_displayname
+  FROM users
+  LEFT JOIN users as invite
+  ON invite.user_id = users.user_invited_from`
+);
+
+export const getUserWithInviteeByID = new DBQuery<
+  Pick<GIDData.user, "user_id">,
+  GIDData.user & { invitee_user_name: GIDData.user["user_name"]; invitee_user_displayname: GIDData.user["user_displayname"] }
+>(
+  `SELECT
+    users.*,
+    invite.user_name as invitee_user_name,
+    invite.user_displayname as invitee_user_displayname
+  FROM users
+  LEFT JOIN users as invite
+  ON invite.user_id = users.user_invited_from
+  WHERE users.user_id = @user_id`
+);
