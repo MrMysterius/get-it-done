18d8850b63fba5aae76abfe10632212f8ce53a73
MrMysterius <official.mrmysterius@gmail.com>
2024-09-01 20:13:05 +0200

    feat: check for token secret in environment variables

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,13 @@
+import { building } from '$app/environment';
 import { databaseSetup } from '$lib/server/database-setup';
 
 await databaseSetup();
+
+if (!building) {
+	if (process.env.TOKEN_SECRET == undefined) {
+		console.error(
+			'[ERROR] Environment Variable TOKEN_SECRET is not set. Which has to be in order for the authentication to work.'
+		);
+		process.exit(1);
+	}
+}
