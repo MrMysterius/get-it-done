eab1682d8af8ccbccbef984d32e8cf29f220f52a
MrMysterius <official.mrmysterius@gmail.com>
2024-09-02 14:30:02 +0200

    feat: authentication check

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,24 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { Prisma } from '@prisma/client';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			auth: AuthenticatedUser;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+
+		interface AuthenticatedUser {
+			isAuthed: boolean;
+			user?: Prisma.userGetPayload<null>;
+			isATExpired?: boolean;
+			isRTExpired?: boolean;
+		}
 	}
 }
 
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,61 @@
+import { redirect, type Handle, type RequestEvent } from '@sveltejs/kit';
+
+import { TOKEN_SECRET } from '$env/static/private';
 import { building } from '$app/environment';
 import { databaseSetup } from '$lib/server/database-setup';
+import prisma from '$lib/server/prisma';
+import { validateJWT } from '$lib/server/functions/validateJWT';
 
 await databaseSetup();
 
 if (!building) {
-	if (process.env.TOKEN_SECRET == undefined) {
+	if (TOKEN_SECRET == undefined) {
 		console.error(
 			'[ERROR] Environment Variable TOKEN_SECRET is not set. Which has to be in order for the authentication to work.'
 		);
 		process.exit(1);
 	}
+}
+
+export const handle: Handle = async ({ event, resolve }) => {
+	await authenticate(event);
+
+	if (!event.locals.auth.isAuthed && !isPublicRoute(event.url.pathname)) {
+		return redirect(307, '/error/401');
+	}
+
+	const response = await resolve(event);
+	return response;
+};
+
+async function authenticate(e: RequestEvent) {
+	const at = e.cookies.get('at') || '';
+	const rt = e.cookies.get('rt') || '';
+
+	const vat = validateJWT(at);
+	const vrt = validateJWT(rt);
+
+	if (!vat.payload || !vrt.payload) {
+		e.locals.auth = { isAuthed: false };
+		return;
+	}
+
+	const user = await prisma.user.findFirst({ where: { id: vat.payload.id } });
+
+	e.locals.auth = {
+		isAuthed: user && !vat.expired ? true : false,
+		user: user || undefined,
+		isATExpired: vat.expired,
+		isRTExpired: vrt.expired
+	};
+
+	return;
+}
+
+const public_paths = ['/', '/api', '/error'];
+
+function isPublicRoute(path: string) {
+	return public_paths.some(
+		(public_path) => path === public_path || path.startsWith(public_path + '/')
+	);
 }
