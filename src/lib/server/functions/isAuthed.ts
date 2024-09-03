f140234eac171c2abe58e3207e64daa6f70d4f10
MrMysterius <official.mrmysterius@gmail.com>
2024-09-03 12:31:12 +0200

    fix: authed check to actually throw errors

--- src/lib/server/functions/isAuthed.ts
+++ src/lib/server/functions/isAuthed.ts
@@ -1,12 +1,12 @@
-import { json, type RequestEvent } from '@sveltejs/kit';
+import { error, type RequestEvent } from '@sveltejs/kit';
 
 export function isAuthed(e: RequestEvent) {
 	if (!e.locals.auth.isAuthed) {
 		if (e.locals.auth.isATExpired && !e.locals.auth.isRTExpired) {
-			return json({ message: 'Current Access Token Expired' }, { status: 401 });
+			return error(401, { message: 'Current Access Token Expired' });
 		}
 
-		return json({ message: 'Unauthorized Request' }, { status: 401 });
+		return error(401, { message: 'Unauthorized Request' });
 	}
 
 	return;
