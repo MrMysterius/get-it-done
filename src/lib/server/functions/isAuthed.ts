da94944effe5456aa2bf261ae07ad81084ae8de8
MrMysterius <official.mrmysterius@gmail.com>
2024-09-04 13:45:40 +0200

    fix: isAuthed to also check if user exists

--- src/lib/server/functions/isAuthed.ts
+++ src/lib/server/functions/isAuthed.ts
@@ -1,7 +1,7 @@
 import { error, type RequestEvent } from '@sveltejs/kit';
 
 export function isAuthed(e: RequestEvent) {
-	if (!e.locals.auth.isAuthed) {
+	if (!e.locals.auth.isAuthed || !e.locals.auth.user) {
 		if (e.locals.auth.isATExpired && !e.locals.auth.isRTExpired) {
 			return error(401, { message: 'Current Access Token Expired' });
 		}
